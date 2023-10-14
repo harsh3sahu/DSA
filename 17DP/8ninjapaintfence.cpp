5b1ba6c57da6ae2e791bd55f49c27026bcc39f18
harsh3sahu <103841259+harsh3sahu@users.noreply.github.com>
2023-10-14 11:08:34 +0530

    ninja fence

--- 17DP/8ninjapaintfence.cpp
+++ 17DP/8ninjapaintfence.cpp
@@ -1,30 +1,74 @@
-// C++ program for Painting Fence Algorithm
-// optimised version
+#include <bits/stdc++.h> 
+#define MOD 1000000007
 
-#include <bits/stdc++.h>
-using namespace std;
-
-// Returns count of ways to color k posts
-long countWays(int n, int k)
-{
-	long dp[n + 1];
-	memset(dp, 0, sizeof(dp));
-	long long mod = 1000000007;
-
-	dp[1] = k;
-	dp[2] = k * k;
-
-	for (int i = 3; i <= n; i++) {
-		dp[i] = ((k - 1) * (dp[i - 1] + dp[i - 2])) % mod;
-	}
-
-	return dp[n];
+int add(int a,int b){
+    return ( (a%MOD)+(b%MOD))%MOD;
 }
 
-// Driver code
-int main()
-{
-	int n = 5, k = 3;
-	cout << countWays(n, k) << endl;
-	return 0;
+int mul(int a ,int b){
+    return ((a%MOD)*1LL*(b%MOD))%MOD;
+    
+}
+
+// int solveRec(int n, int k){
+//     if(n==1){
+//         return k;
+//     }
+//     if(n==2){
+//         return k*k;
+//     }
+    
+    
+//     int ans= add(mul(solveRec(n-2,k),k-1),mul(solveRec(n-1,k),k-1));
+
+//     return ans;
+
+    
+// }
+
+// int solveMem(int n, int k,vector<int> &dp){
+//     if(n==1){
+//         return k;
+//     }
+//     if(n==2){
+//         return k*k;
+//     }
+//     if(dp[n]!=-1){
+//         return dp[n];
+//     }
+    
+//     dp[n]= add(mul(solveMem(n-2,k,dp),k-1),mul(solveMem(n-1,k,dp),k-1));
+
+//     return dp[n];
+
+    
+// }
+
+
+int solveTab(int n, int k){
+    vector<int> dp(n+1,0);
+    
+   long long int  prev2=k;
+    long long int prev1=add(k,mul(k,k-1));
+    
+    for(int i=3;i<=n;i++){
+        long long int temp =add (mul(prev2,k-1)%MOD,mul(prev1,k-1) %MOD) %MOD;
+        prev2=prev1;
+        prev1=temp;
+    }
+    return prev1;
+}
+
+
+int numberOfWays(int n, int k) {
+    // Write your code here.
+    // int ans=solveRec(n,k);
+    // return ans;
+
+    // vector<int> dp(n+1,-1);
+    // return solveMem(n,k,dp);
+
+    int ans=solveTab( n, k);
+    return ans;
+    
 }
